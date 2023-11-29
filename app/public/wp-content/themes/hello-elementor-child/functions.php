f665b2f874cc9a6596541b0501e03db951133317
Artax601 <artaxw1@gmail.com>
2023-11-29 14:49:19 +0100

    Function update hook

--- app/public/wp-content/themes/hello-elementor-child/functions.php
+++ app/public/wp-content/themes/hello-elementor-child/functions.php
@@ -23,13 +23,13 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 
 function admin_link_shortcode() {
-    ob_start(); // Démarre la temporisation de la sortie
-    if (is_user_logged_in()) {
-        echo '<a href="' . admin_url() . '" class="admin-link">Admin</a>';
-        }
-    return ob_get_clean(); // Récupère le contenu du tampon de sortie et le nettoie
+    if (is_user_logged_in()) { // Je vérifie si l'utilisateur et connecté à Wordpress
+        return '<a href=" '. admin_url() .' " class="admin-link">Admin</a>';
+    }
+    return ''; // Je retourne un emplacement vide si l'utilisateur n'est pas connecté à Wordpress
 }
-add_shortcode('admin_link', 'admin_link_shortcode');
+add_shortcode('admin_link', 'admin_link_shortcode'); // Ajout d'un Shortcode pour pouvoir l'intégrer sur mon header
+
 
 
 
